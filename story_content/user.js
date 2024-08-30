06fd949275d9b2552624acd8b5dcff7fe39c0b56
Jane Nerizon <janenerizon93@gmail.com>
2024-08-30 15:49:12 +0800

    v1

---
+++ story_content/user.js
@@ -0,0 +1,127 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5y2KGZ7qTIe":
+        Script1();
+        break;
+      case "6E7xkqecuH0":
+        Script2();
+        break;
+      case "5jIKB10pXpP":
+        Script3();
+        break;
+      case "5X5ziJ8mZuZ":
+        Script4();
+        break;
+      case "66bHQCKmRoc":
+        Script5();
+        break;
+      case "5ZVgjIJQuxG":
+        Script6();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var body = document.querySelector("body") 
+var style = document.createElement('style') 
+style.type = 'text/css'; 
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) || 
+(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.platform))) { 
+// mobile;   
+style.appendChild(document.createTextNode(".caption p {height:auto; max-width:80%;padding: 5px 7px 5px 7px !important; margin: 0px 0px 0px 0px !important; background: rgba(217, 217, 217, 0.9); border: 1px solid #D9D9D9; color: #212121; text-decoration: none; text-shadow: none; border-radius: 0px !important; font-size: 14px; font-family: Arial, Helvetica, sans-serif; } .theme-unified .caption { top:80%; width:100%; height:auto; border: 1px solid rgba(255,255,255,0.001); position:absolute !important; display:block !important;   }" ));     
+}else{ 
+// desktop 
+style.appendChild(document.createTextNode(" .caption p {font-size: calc(13px + 0.4rem);  height: 5vh; min-height:40px; background: rgba(217, 217, 217, 0.9); border: 1px solid #D9D9D9; color: #212121; text-decoration: none; text-shadow: none; border-radius: 0px !important; font-size: 18px; font-family: Arial, Helvetica, sans-serif;} .view-desktop .caption, .theme-unified .caption {width:100%; height:8.5%; border: 2px solid rgba(255,255,255,.001); }" ));     
+}   
+body.appendChild(style);
+}
+
+window.Script2 = function()
+{
+  var x = document.getElementById("seek"); 
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ||  
+
+    (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.platform))) {  
+
+x.style.display = "none"; 
+
+}else{ 
+
+x.style.display = "block"; 
+}
+
+window.Script3 = function()
+{
+  var body = document.querySelector("body") 
+var style = document.createElement('style') 
+style.type = 'text/css'; 
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) || 
+(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.platform))) { 
+// mobile;   
+style.appendChild(document.createTextNode(".caption p {height:auto; max-width:80%;padding: 5px 7px 5px 7px !important; margin: 0px 0px 0px 0px !important; background: rgba(217, 217, 217, 0.9); border: 1px solid #D9D9D9; color: #212121; text-decoration: none; text-shadow: none; border-radius: 0px !important; font-size: 14px; font-family: Arial, Helvetica, sans-serif; } .theme-unified .caption { top:80%; width:100%; height:auto; border: 1px solid rgba(255,255,255,0.001); position:absolute !important; display:block !important;   }" ));     
+}else{ 
+// desktop 
+style.appendChild(document.createTextNode(" .caption p {font-size: calc(13px + 0.4rem);  height: 5vh; min-height:40px; background: rgba(217, 217, 217, 0.9); border: 1px solid #D9D9D9; color: #212121; text-decoration: none; text-shadow: none; border-radius: 0px !important; font-size: 18px; font-family: Arial, Helvetica, sans-serif;} .view-desktop .caption, .theme-unified .caption {width:100%; height:8.5%; border: 2px solid rgba(255,255,255,.001); }" ));     
+}   
+body.appendChild(style);
+}
+
+window.Script4 = function()
+{
+  var body = document.querySelector("body") 
+var style = document.createElement('style') 
+style.type = 'text/css'; 
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) || 
+(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.platform))) { 
+// mobile;   
+style.appendChild(document.createTextNode(".caption p {height:auto; max-width:80%;padding: 5px 7px 5px 7px !important; margin: 0px 0px 0px 0px !important; background: rgba(217, 217, 217, 0.9); border: 1px solid #D9D9D9; color: #212121; text-decoration: none; text-shadow: none; border-radius: 0px !important; font-size: 14px; font-family: Arial, Helvetica, sans-serif; } .theme-unified .caption { top:80%; width:100%; height:auto; border: 1px solid rgba(255,255,255,0.001); position:absolute !important; display:block !important;   }" ));     
+}else{ 
+// desktop 
+style.appendChild(document.createTextNode(" .caption p {font-size: calc(13px + 0.4rem);  height: 5vh; min-height:40px; background: rgba(217, 217, 217, 0.9); border: 1px solid #D9D9D9; color: #212121; text-decoration: none; text-shadow: none; border-radius: 0px !important; font-size: 18px; font-family: Arial, Helvetica, sans-serif;} .view-desktop .caption, .theme-unified .caption {width:100%; height:8.5%; border: 2px solid rgba(255,255,255,.001); }" ));     
+}   
+body.appendChild(style);
+}
+
+window.Script5 = function()
+{
+  var x = document.getElementById("seek"); 
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ||  
+
+    (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.platform))) {  
+
+x.style.display = "none"; 
+
+}else{ 
+
+x.style.display = "block"; 
+}
+
+window.Script6 = function()
+{
+  var body = document.querySelector("body") 
+var style = document.createElement('style') 
+style.type = 'text/css'; 
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) || 
+(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.platform))) { 
+// mobile;   
+style.appendChild(document.createTextNode(".caption p {height:auto; max-width:80%;padding: 5px 7px 5px 7px !important; margin: 0px 0px 0px 0px !important; background: rgba(217, 217, 217, 0.9); border: 1px solid #D9D9D9; color: #212121; text-decoration: none; text-shadow: none; border-radius: 0px !important; font-size: 14px; font-family: Arial, Helvetica, sans-serif; } .theme-unified .caption { top:80%; width:100%; height:auto; border: 1px solid rgba(255,255,255,0.001); position:absolute !important; display:block !important;   }" ));     
+}else{ 
+// desktop 
+style.appendChild(document.createTextNode(" .caption p {font-size: calc(13px + 0.4rem);  height: 5vh; min-height:40px; background: rgba(217, 217, 217, 0.9); border: 1px solid #D9D9D9; color: #212121; text-decoration: none; text-shadow: none; border-radius: 0px !important; font-size: 18px; font-family: Arial, Helvetica, sans-serif;} .view-desktop .caption, .theme-unified .caption {width:100%; height:8.5%; border: 2px solid rgba(255,255,255,.001); }" ));     
+}   
+body.appendChild(style);
+}
+
+};
